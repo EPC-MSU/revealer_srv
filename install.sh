3aa534426000ae0c0fbc6e3361d6cda63c32396e
tarabrina_n <tarabrina_n@physlab.ru>
2024-05-07 12:28:13 +0300

    Install script corrected to correctly perform reinstallation and print more information. #96884

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+echo "Starting the installation of PySSDP Server..."
 # if this service is already in work - stop it
 if [ -e /etc/systemd/system/pyssdp_server.service ]
 then
+    echo "PySSDP Server is already installed so it will now be reinstalled."
     systemctl stop pyssdp_server
     systemctl disable pyssdp_server
 fi
@@ -13,15 +15,22 @@ rm -rf /usr/lib/pyssdp_server/webroot
 # copy folder with service source files
 cp -a ./ /usr/lib/pyssdp_server
 
+echo "Copying server file to the /usr/lib/pyssdp_server/ directory..."
+# remove venv if exist
+rm -rf /usr/lib/pyssdp_server/venv
+
+echo "Creating a virtual environment..."
 # create venv with requirements
 cd /usr/lib/pyssdp_server
 python3 -m venv venv
 venv/bin/python3 -m pip install --upgrade pip
 venv/bin/python3 -m pip install -r requirements.txt
 
-# copy 
+echo "Creating and starting a systemd service..."
+# copy
 cp pyssdp_server.service /etc/systemd/system
 
 # enable service
 systemctl enable pyssdp_server
 systemctl start pyssdp_server
+
--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-# disable service
-systemctl stop pyssdp_server
-systemctl disable pyssdp_server
+if [ -e /etc/systemd/system/pyssdp_server.service ]
+then
+    echo "Stopping and disabling PySSDP Server service..."
+    # disable service
+    systemctl stop pyssdp_server
+    systemctl disable pyssdp_server
 
-# remove service source files from the system
-rm -rf /usr/lib/pyssdp_server
-rm -rf /etc/systemd/system/pyssdp_server.service
+
+    echo "Removing its files..."
+    # remove service source files from the system
+    rm -rf /usr/lib/pyssdp_server
+    rm -rf /etc/systemd/system/pyssdp_server.service
+
+    echo "PySSDP Server is now uninstalled from the system."
+else
+    echo "PySSDP Server is not installed."
+fi
